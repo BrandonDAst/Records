55dce0c72fe1e2168ca37c87feb3be5b13016f07
brandon <brandon.diaz.as@gmail.com>
2020-01-14 11:34:39 -0600

    Initial Commit

---
+++ RecordsAPI/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Data.SqlClient;
+using RecordsAPI.Models;
+
+namespace RecordsAPI
+{
+    public class Record
+    {
+        public int Id { get; private set; }
+        protected int UserId = 5; //Configurated via session
+        public bool Enabled { get; private set; }
+        static string _dataBase;
+
+        protected virtual bool Update(Record newData){ return false; }
+        protected static Record GerRecord(int Id)
+        {
+            Record r = new Record();
+            using (SqlConnection connection = new SqlConnection(_dataBase))
+            {
+                
+            }
+            return r;
+        }
+    }
+
+    public class Product : Record
+    {
+        public string Description { get; set; }
+        public string Brand { get; set; }
+        private void SetDefaults()
+        {
+        }
+        public Product(string description, string brand, int userId)
+        {
+            SetDefaults();
+            Description = description;
+            Brand = brand;
+        }
+    }
+
+    public class Test 
+    {
+        private void CreateRecord()
+        {
+            Product product = new Product("","",1);
+        }
+    }
+}
---
+++ RecordsAPI/Models/IRecord.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecordsAPI.Models
+{
+    interface IRecord
+    {
+        //int Id { get; set; }
+        //int UserId { get; set; }
+        //bool Enabled { get; set; }
+        //string _dataBase { set; }
+        //Record GerRecord(int Id);
+        //bool Update(Record nuevo);
+    }
+}
